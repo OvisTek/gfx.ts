1d1a96341ba7537e7597579d32da0b86003e64b6
David Arayan <david.arayan12@gmail.com>
2020-12-23 00:25:24 +1100

    - Added MathUtil.floatToIntBits() to replicate Java functionality - Added Transform component

--- src/math/math-util.ts
+++ src/math/math-util.ts
@@ -7,6 +7,11 @@ export class MathUtil {
     /** Multiply to convert from degrees to radians */
     public static readonly DEGTRAD: number = Math.PI / 180.0;
 
+    // used internally for some bit-shift operations
+    private static readonly INT8 = new Int8Array(4);
+    private static readonly INT32 = new Int32Array(MathUtil.INT8.buffer, 0, 1);
+    private static readonly FLOAT32 = new Float32Array(MathUtil.INT8.buffer, 0, 1);
+
     /**
      * Clamps a number between min and max
      * @param value The value to clamp
@@ -18,5 +23,17 @@ export class MathUtil {
         if (value > max) { return max };
 
         return value;
+    }
+
+    /**
+     * Convert a provided number into its integer bit representation
+     * This is useful for certain hashing operations
+     * 
+     * @param value The value to convert
+     */
+    public static floatToIntBits(value: number): number {
+        MathUtil.FLOAT32[0] = value;
+
+        return MathUtil.INT32[0];
     }
 }
---
+++ src/renderer/transform.ts
@@ -0,0 +1,152 @@
+import { Euler } from "../math/euler";
+import { MathUtil } from "../math/math-util";
+import { Matrix4 } from "../math/matrix4";
+import { Quaternion } from "../math/quaternion";
+import { Vector3 } from "../math/vector3";
+
+/**
+ * Transform is responsible for allowing positioning, rotating and scaling of
+ * 3D objects in the Renderer.
+ * 
+ * When assigning new values for a Transform, the values are copied
+ */
+export class Transform {
+    // these are the components that should be used to control this transform
+    private readonly _position: Vector3;
+    private readonly _rotation: Quaternion;
+    private readonly _scale: Vector3;
+    private readonly _euler: Euler;
+
+    // these components are updated automatically based on the scene-graph
+    private readonly _localMatrix: Matrix4;
+    private readonly _worldMatrix: Matrix4;
+
+    // the previously computed hash of this Transform
+    private _hash: number = 0;
+    private _parentHash: number = 0;
+
+    constructor() {
+        this._position = new Vector3();
+        this._rotation = new Quaternion();
+        this._scale = new Vector3(1, 1, 1);
+        this._localMatrix = new Matrix4();
+        this._worldMatrix = new Matrix4();
+    }
+
+    public get position(): Vector3 {
+        return this._position;
+    }
+
+    public set position(value: Vector3) {
+        this._position.copy(value);
+    }
+
+    public get rotation(): Quaternion {
+        return this._rotation;
+    }
+
+    public set rotation(value: Quaternion) {
+        this._rotation.copy(value);
+    }
+
+    public get scale(): Vector3 {
+        return this._scale;
+    }
+
+    public set scale(value: Vector3) {
+        this._scale.copy(value);
+    }
+
+    public get euler(): Euler {
+        this._rotation.euler(this._euler);
+
+        return this._euler;
+    }
+
+    public set euler(value: Euler) {
+        value.quaternion(this._rotation);
+    }
+
+    /**
+     * Returns the 4x4 Matrix reference that represents this Transform in local/object space
+     * 
+     * NOTE: Unless manual update is enabled, the contents of this matrix will override each frame
+     */
+    public get localMatrix(): Matrix4 {
+        return this._localMatrix;
+    }
+
+    /**
+     * Returns the 4x4 Matrix reference that represents this Transform in world space
+     * 
+     * NOTE: The contents of this matrix will override each frame. For root objects, the localMatrix is
+     * also the world matrix.
+     */
+    public get worldMatrix(): Matrix4 {
+        return this._worldMatrix;
+    }
+
+    /**
+     * Returns the previously computed hash of this Transform derived from
+     * its components
+     */
+    public get hash(): number {
+        return this._hash;
+    }
+
+    /**
+     * Generates a hash-code of this Transform. The hash can be used to check if
+     * the transform has actually updated or requires some form of update
+     */
+    public hashCode(): number {
+        const prime: number = 31;
+        let result: number = 1;
+
+        // hash the position
+        result = prime * result + MathUtil.floatToIntBits(this._position.x);
+        result = prime * result + MathUtil.floatToIntBits(this._position.y);
+        result = prime * result + MathUtil.floatToIntBits(this._position.z);
+
+        // hash the rotation
+        result = prime * result + MathUtil.floatToIntBits(this._rotation.x);
+        result = prime * result + MathUtil.floatToIntBits(this._rotation.y);
+        result = prime * result + MathUtil.floatToIntBits(this._rotation.z);
+        result = prime * result + MathUtil.floatToIntBits(this._rotation.w);
+
+        // hash the scale
+        result = prime * result + MathUtil.floatToIntBits(this._scale.x);
+        result = prime * result + MathUtil.floatToIntBits(this._scale.y);
+        result = prime * result + MathUtil.floatToIntBits(this._scale.z);
+
+        return result;
+    }
+
+    /**
+     * Called by the renderer to update the world position of this transform.
+     * Passes the transform that represents the parent/root of this transform
+     * 
+     * @param parent The parent transform that should be used to update this transform
+     */
+    public apply(parent: Transform): void {
+        const computedHash: number = this.hashCode();
+
+        // this means the transform has changed since the last update, we need to re-update
+        // the local matrix
+        if (computedHash != this._hash) {
+            this._localMatrix.composePosRotSca(this._position, this._rotation, this._scale);
+        }
+
+        const parentHash = parent.hash;
+
+        // we update the world matrix only if either the local transform has changed
+        // or the parent transform has changed
+        if (computedHash != this._hash || parentHash != this._parentHash) {
+            // update the world matrix of this transform from parent
+            Matrix4.multiply(this._localMatrix, parent.worldMatrix, this._worldMatrix);
+        }
+
+        // update hash codes for the next apply() call
+        this._hash = computedHash
+        this._parentHash = parentHash;
+    }
+}
