cd88a1b54c309156984719d5cf6fbb86295490b2
David Arayan <david.arayan12@gmail.com>
2020-12-23 19:03:39 +1100

    - Added the basic framework for basic execution framework

---
+++ src/renderer/scriptable/stage-object.ts
@@ -0,0 +1,183 @@
+import { Stage } from "../stage";
+import { Transform } from "../transform";
+
+/**
+ * Everything extends the StageObject as a base class. Contains a number of 
+ */
+export abstract class StageObject {
+    // this is where the stage object will be rendered in the scene
+    private readonly _transform: Transform;
+    private _stage?: Stage;
+
+    constructor() {
+        this._transform = new Transform();
+        this._stage = undefined;
+    }
+
+    /**
+     * Called by the Rendering Engine to construct this object asynchronously. Object
+     * will not be executed/rendered until the promise is resolved
+     */
+    protected create(): Promise<void> {
+        return new Promise((accept, reject) => {
+            return accept();
+        });
+    }
+
+    /**
+     * Returns the Stage that this object is attached to
+     */
+    protected get stage(): Stage {
+        if (!this._stage) {
+            throw new Error("StageObject.stage() - object was not setup correctly, context unavailable");
+        }
+        return this._stage;
+    }
+
+    /**
+     * Returns the Transform that allows moving, rotating or scaling this object
+     */
+    protected get transform(): Transform {
+        return this._transform;
+    }
+
+    /**
+     * Called by the Rendering Engine just before a rendering is about to be done
+     * 
+     * @param deltaTime The time difference between the last and current frame in seconds
+     */
+    protected update(deltaTime: number) { }
+
+    /**
+     * Called by the Rendering Engine just after a rendering was done
+     * 
+     * @param deltaTime The time difference between the last and current frame in seconds
+     */
+    protected lateUpdate(deltaTime: number) { }
+
+    /**
+     * Called by the Rendering Engine when it was paused by a user or a script
+     */
+    protected onPause() { }
+
+    /**
+     * Called by the Rendering Engine when it was resumed from a previously paused state
+     */
+    protected onResume() { }
+
+    /**
+     * Called by the Rendering Engine when this object was removed from the Stage
+     */
+    protected onDestroy() { }
+
+    /**
+     * Called by the Rendering Engine when the Canvas/Renderer was resized to a new size
+     * 
+     * @param newWidth The new width of the Renderer/Canvas
+     * @param newHeight The new height of the Renderer/Canvas
+     */
+    protected onResize(newWidth: number, newHeight: number) { }
+
+    /**
+     * Safe execution of the create() function. This should not be called from user-space
+     * 
+     * @param stage The Stage reference
+     */
+    public _exec_Create(stage: Stage): Promise<void> {
+        // sets the stage reference
+        this._stage = stage;
+
+        return this.create();
+    }
+
+    /**
+     * Safe execution of the update() function. This should not be called from user-space
+     * 
+     * @param deltaTime The time difference between the last and current frame in seconds
+     */
+    public _exec_Update(deltaTime: number): Error | undefined {
+        try {
+            this.update(deltaTime);
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Safe execution of the lateUpdate() function. This should not be called from user-space
+     * 
+     * @param deltaTime The time difference between the last and current frame in seconds
+     */
+    public _exec_LateUpdate(deltaTime: number): Error | undefined {
+        try {
+            this.lateUpdate(deltaTime);
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Safe execution of the onPause() function. This should not be called from user-space
+     */
+    public _exec_onPause(): Error | undefined {
+        try {
+            this.onPause();
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Safe execution of the onResume() function. This should not be called from user-space
+     */
+    public _exec_onResume(): Error | undefined {
+        try {
+            this.onResume();
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Safe execution of the onDestroy() function. This should not be called from user-space
+     */
+    public _exec_onDestroy(): Error | undefined {
+        try {
+            this.onDestroy();
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+
+    /**
+     * Safe execution of the onResize() function. This should not be called from user-space
+     * 
+     * @param newWidth The new width of the Renderer/Canvas
+     * @param newHeight The new height of the Renderer/Canvas
+     */
+    protected _exec_OnResize(newWidth: number, newHeight: number): Error | undefined {
+        try {
+            this.onResize(newWidth, newHeight);
+        }
+        catch (error) {
+            return error;
+        }
+
+        return undefined;
+    }
+}
